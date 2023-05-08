c02c21d89274112e2395fa45c22442e197c3d558
손현지 <108772553+sonhyunji@users.noreply.github.com>
2023-05-08 22:48:01 +0900

    게시글 등록, 수정, 삭제 가능하고 게시글 검색도 가능. 가장 최근에 등록된 게시글을 보여준다. 게시글 링크 클릭 시에는 상세 페이지도 보여줌.

--- src/main/java/com/plat/plantsplanet/repository/BoardRepository.java
+++ src/main/java/com/plat/plantsplanet/repository/BoardRepository.java
@@ -1,9 +1,16 @@
 package com.plat.plantsplanet.repository;
 
 import com.plat.plantsplanet.entity.Board;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+
 @Repository
 public interface BoardRepository extends JpaRepository<Board, Integer> {
+    Page<Board> findByTitleContaining(String searchKeyword, Pageable pageable);
+
+    Page<Board> findAll(Pageable pageable);
+
 }
